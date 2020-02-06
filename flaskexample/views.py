887d0b8017f2bcdb9aa0c50de4454cb48b83cffc
Ferrandino <ubuntu@ip-172-31-36-221.us-east-2.compute.internal>
2020-02-06 19:37:29 +0000

    add files

---
+++ flaskexample/views.py
@@ -0,0 +1,108 @@
+from flask import render_template
+from flask import request
+from flaskexample import app
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
+import os
+import urllib.request
+import requests
+from flask import send_from_directory, flash, redirect
+from functions_new import *
+# from sqlalchemy import create_engine
+# from sqlalchemy_utils import database_exists, create_database
+# import pandas as pd
+# import psycopg2
+UPLOAD_FOLDER = '/home/ubuntu/flaskapp-master/flaskexample/static/data/upload_folder/'
+UPLOAD_POST_FOLDER = '/home/ubuntu/flaskapp-master/flaskexample/static/data/upload_folder/'
+IMAGE_FOLDER ='./static/class_art/'
+
+recom_path = '/home/ubuntu/flaskapp-master/flaskexample/static/data/art_for_app.csv'
+pd.set_option('display.max_colwidth', 1000)
+art = pd.read_csv(recom_path, index_col = 0)
+
+
+#app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_POST_FOLDER'] = UPLOAD_POST_FOLDER
+app.config['IMAGE_DIR'] = IMAGE_FOLDER
+app.config.update(
+    TESTING=True,
+    SECRET_KEY=b'secret_key'
+)
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('input.html')
+
+
+@app.route('/recommendations', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'GET':
+        return render_template('input.html')
+
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            print('No file 1')
+            return render_template('input.html')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            print('No file 2')
+            return render_template('input.html')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #file saved to upload folder
+            #load model            
+            #get image ready
+            model = get_model()            
+            processedimage = read_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #detect feature of new image
+            feature_dict,size = extract_img_features(model,os.path.join(app.config['UPLOAD_FOLDER'], filename),processedimage)
+            #calc lsh
+            lsh = calc_LSH(feature_dict,size)
+            #return 3 closest things
+            F,F1,F2,F3 = get_similar_item(os.path.join(app.config['UPLOAD_FOLDER'], filename), feature_dict, lsh, 3)
+            #return the links
+            F1_base, F2_base, F3_base, x1, x2, x3 = get_link(F1,F2,F3)
+
+            #group ouput
+#	    panel_img = [F1_base, F2_base, F3_base]
+            panel_img = [F1_base,F2_base,F3_base]
+            print(panel_img)
+            panel_link = [x1, x2, x3]
+            print(panel_link)
+#            print(panel_link)
+#            print(panel_link)
+
+            linktext = "Select"
+            original = os.path.join(app.config['UPLOAD_POST_FOLDER'], filename)
+            panel_img_path = [os.path.join(app.config['IMAGE_DIR'],x) for x in panel_img]
+            print(original)
+            print(panel_img_path)
+            returnval = render_template('recommendations.html', panel_img_path = panel_img_path, original = original, panel_link = panel_link, linktext = linktext, piclink = art['link'].iloc[0])
+           
+        return returnval
+    
+            
+            
+    return render_template('input.html')
+
+if __name__ == "__main__":
+	app.run(debug=True)
+#def go():
+#    query = request.args.get('query', '')
+#    return render_template(
+#        'recommendations.html',
+#        query=query,
+#    )
---
+++ functions_new.py
@@ -0,0 +1,138 @@
+# basic functions & analytics
+
+import os
+import numpy as np
+import pandas as pd
+import ntpath
+
+# making predictions & extracting features with model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
+from keras.backend import clear_session
+import tensorflow as tf
+
+# calculating image similarity
+# calculating image similarity
+from lshash_2.lshash_2 import LSHash
+from tqdm import notebook
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# pinging SQL
+#from sqlalchemy import create_engine
+#from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+
+# loading classified image features
+import pickle
+#-------------------------------------------------------------------------------
+
+mainpath = '/home/ubuntu/flaskapp-master/flaskexample'
+user_image_path = '/home/ubuntu/flaskapp-master/flaskexample/static/data/upload_folder/'
+CSV = '/home/ubuntu/flaskapp-master/flaskexample/static/data/art_for_app.csv'
+
+
+#os.getcwd()
+#modelfile = os.getcwd()+"/models/VGG_cat.smallerLR.h5"
+#clear_session()
+#model = load_model(modelfile)
+
+
+def get_model():
+    global model
+    model = load_model('VGG_cat.smallerLR.h5')
+    print('* Model loaded!')
+    return model
+
+
+def read_image(file):
+    img = image.load_img(file,target_size=(150,150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    processedimage = preprocess_input(x)
+    return processedimage
+    
+print("* Loading Keras model...")
+get_model()
+graph = tf.get_default_graph()
+
+#def extract_img_features(model,upload_image_path,processedimage):
+#    feature_dict = pickle.load(open(mainpath+"/models/VGG_cat_layer3.p","rb"))
+#    get_layer = K.function([model.input],[model.layers[3].output])
+#    newimgfeature = get_layer([processedimage])[0].flatten()
+#    feature_dict[upload_image_path] = newimgfeature
+#    size = np.shape(newimgfeature)
+#    return feature_dict,size
+
+def extract_img_features(model,upload_image_path,processedimage):
+    feature_dict = pickle.load(open(mainpath+"/models/VGG_cat_layer3.p","rb"))
+    get_layer = K.function([model.input],[model.layers[3].output])
+    newimgfeature = get_layer([processedimage])[0].flatten()
+    feature_dict[upload_image_path] = newimgfeature
+    size = np.shape(newimgfeature)
+    return feature_dict,size
+
+
+def calc_LSH(feature_dict, size):
+    featuresize = np.shape(feature_dict)
+
+    # params
+    k = 5 # hash size
+    L = 5  # number of tables
+    d = size[0] # Dimension of Feature vector
+    lsh = LSHash(hash_size=k, input_dim=d, num_hashtables=L)
+
+    # LSH on all the images
+    for img_path, vec in notebook.tqdm(feature_dict.items()):
+        lsh.index(vec, extra_data=img_path)
+    
+    return lsh
+
+def get_similar_item(file, feature_dict, lsh_variable, n_items):
+    idx =list(feature_dict.keys()).index(file)
+    response = lsh_variable.query(feature_dict[list(feature_dict.keys())[idx]].flatten(),
+                 num_results=n_items+1, distance_func='cosine')
+    F = response[0][0][1]
+    F1 = response[1-2][0][1]
+    F2 = response[1-3][0][1]
+    F3 = response[1-4][0][1]
+    return F,F1,F2,F3
+    
+def find_price_url(F1):
+
+    dbname = 'art'
+    username = 'postgres' # change this to your username
+    pw = 'postgres'
+
+    db = create_engine('postgres://%s:%s@localhost/%s'%(username,pw,dbname))
+#    print(engine.url)
+    con = None
+    con = psycopg2.connect(database = dbname, user = username, host='localhost',  password = pw)
+
+    listing = []
+    # query:
+    sql_query = """
+    SELECT link FROM art_table WHERE item=F1_base;
+    """
+    listing.append(pd.read_sql_query(sql_query,con))
+
+    listing = pd.concat(listing)
+    #listing['listing_id'] = similar_listings[1::]
+
+    return listing
+
+
+def get_link(F1,F2,F3):
+    pd.set_option('display.max_colwidth', 1000)
+    df = pd.read_csv(CSV,index_col = 0)
+    F1_base = ntpath.basename(F1)
+    F2_base = ntpath.basename(F2)
+    F3_base = ntpath.basename(F3)
+    x1 = df.loc[df['item'] == F1_base, 'link'].iloc[0]
+    x2 = df.loc[df['item'] == F2_base, 'link'].iloc[0]
+    x3 = df.loc[df['item'] == F3_base, 'link'].iloc[0]
+
+    return F1_base, F2_base, F3_base, x1, x2, x3
+    
